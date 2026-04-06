365b410b68f5f3b4a7d143723dab5034b18af046
FelixBruguera <felixbruguera0@gmail.com>
2026-04-06 10:00:42 -0400

    fix across (#283)
    * incentivesOnly in across
    
    * remove unused imports
    
    * categories
    
    * categories

--- protocols/across.ts
+++ protocols/across.ts
@@ -1,11 +1,6 @@
-import { balance, latest } from "../adapters/balance";
-import { manualCliff } from "../adapters/manual";
 import { CliffAdapterResult, Protocol, SectionV2 } from "../types/adapters";
 import { queryCustom } from "../utils/queries";
 import { readableToSeconds } from "../utils/time";
-
-const start = 1669680000;
-const qty = 1e9;
 
 const liquidityRewards = async (): Promise<CliffAdapterResult[]> => {
   const data = await queryCustom(`
@@ -65,10 +60,14 @@ const incentives: SectionV2 = {
   components: [{
     id: "referral-rewards",
     name: "Referral Rewards",
+    methodology: "Tracks Claimed events from the Across Merkle Distributor contract",
+    isIncentive: true,
     fetch: referralRewards,
   },
     {
       id: "liquidity-rewards",
+      isIncentive: true,
+      methodology: "Tracks RewardsWithdrawn events from the Across Accelerating Distributor contract",
       name: "Liquidity Rewards",
       fetch: liquidityRewards,
     },
@@ -76,72 +75,23 @@ const incentives: SectionV2 = {
 }
 
 const across: Protocol = {
-  Airdrop: manualCliff(start, qty * 0.125),
-  "Strategic Partnerships": [
-    manualCliff("2025-06-30", qty * 0.1),
-    (backfill: boolean) =>
-      balance(
-        ["0x8180D59b7175d4064bDFA8138A58e9baBFFdA44a"],
-        "0x44108f0223A3C3028F5Fe7AEC7f9bb2E66beF82F",
-        "ethereum",
-        "across",
-        1669939200,
-        backfill
-      ),
-  ],
-  "DAO Treasury": (backfill: boolean) =>
-    balance(
-      ["0xB524735356985D2f267FA010D681f061DfF03715"],
-      "0x44108f0223A3C3028F5Fe7AEC7f9bb2E66beF82F",
-      "ethereum",
-      "across",
-      1669642200,
-      backfill
-    ),
-  // "Protocol Rewards": (backfill: boolean) =>
-  //   balance(
-  //     ["0x9040e41eF5E8b281535a96D9a48aCb8cfaBD9a48"],
-  //     "0x44108f0223A3C3028F5Fe7AEC7f9bb2E66beF82F",
-  //     "ethereum",
-  //     "across",
-  //     1669642200,
-  //     backfill
-  //   ),
   "Incentives": incentives,
   meta: {
     notes: [
-      `We have inferred the Protocol Rewards contract addresses from token balances.`,
+      `Incentives include referral and liquidity rewards.`,
     ],
     token: "ethereum:0x44108f0223a3c3028f5fe7aec7f9bb2e66bef82f",
     sources: [
       `https://medium.com/across-protocol/happy-birthday-across-to-we-got-you-something-11dbef976d6a`,
+      "https://etherscan.io/address/0x9040e41ef5e8b281535a96d9a48acb8cfabd9a48",
+      "https://etherscan.io/address/0xe50b2ceac4f60e840ae513924033e753e2366487"
     ],
     version: 2,
     protocolIds: ["1207"],
-    total: qty,
-    incompleteSections: [
-      {
-        lastRecord: (backfill: boolean) => latest("across", 1669642200, backfill),
-        key: "DAO Treasury",
-        allocation: qty * 0.525,
-      },
-      // {
-      //   lastRecord: (backfill: boolean) => latest("across", 1669642200, backfill),
-      //   key: "Protocol Rewards",
-      //   allocation: qty * 0.1,
-      // },
-      {
-        lastRecord: (backfill: boolean) => latest("across", 1669939200, backfill),
-        key: "Strategic Partnerships",
-        allocation: qty * 0.25,
-      },
-    ],
+    incentivesOnly: true
   },
   categories: {
-    airdrop: ["Airdrop"],
-    noncirculating: ["DAO Treasury"],
     farming: ["Referral Rewards", "Liquidity Rewards"],
-    privateSale: ["Strategic Partnerships"],
   },
 };
 
