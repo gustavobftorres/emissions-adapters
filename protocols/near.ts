848bbfe5d2572a1bb1d61ca4dbf1f8bb8d71aa15
FelixBruguera <felixbruguera0@gmail.com>
2026-04-07 11:14:21 -0400

    fix near (#288)

--- protocols/near.ts
+++ protocols/near.ts
@@ -1,28 +1,23 @@
 import { manualCliff, manualLinear } from "../adapters/manual";
-import { LinearAdapterResult, Protocol } from "../types/adapters";
+import { Protocol } from "../types/adapters";
+import { queryDuneSQLCached } from "../utils/dune";
 import { periodToSeconds } from "../utils/time";
 
 const start: number = 1587510000;
 const qty: number = 1e9;
 
-function newIssuance(): LinearAdapterResult[] {
-  const sections: LinearAdapterResult[] = [];
-  let thisStart: number = start;
-  let workingQty: number = qty;
-  for (let i = 0; i < 5; i++) {
-    const amount: number = workingQty * 0.05;
-    const end: number = thisStart + periodToSeconds.year;
-    sections.push({
-      type: "linear",
-      start: thisStart,
-      end,
-      amount,
-    });
-    thisStart = end;
-    workingQty += amount;
-  }
-  return sections;
-}
+const staking = queryDuneSQLCached(`
+  SELECT date, amount FROM (
+    SELECT
+     to_unixtime(date) as date,
+     (CAST(MAX(total_supply) AS DOUBLE) - CAST(LAG(MAX(total_supply)) OVER (ORDER BY date) AS DOUBLE)) / 1e24 as amount
+    FROM near.block_chunks
+    WHERE date >= date_add('day', -1, START)
+    GROUP BY date
+    ORDER BY date
+  ) WHERE amount IS NOT NULL AND date >= START
+ `, start, { protocolSlug: "near", allocation: "Staking Rewards"})
+
 
 const near: Protocol = {
   "Core Team": [
@@ -57,20 +52,31 @@ const near: Protocol = {
     start + periodToSeconds.month * 6,
     3e7,
   ),
+  "Early Ecosystem (Unlocked)": manualCliff(start, 8e7),
+  "Early Ecosystem (Other)": manualLinear(
+    start,
+    start + periodToSeconds.year,
+    7e6,
+  ),
   "Foundation Endowment": [
     manualCliff(start, qty * 0.05),
     manualLinear(start, start + periodToSeconds.year * 2, qty * 0.05),
   ],
+  "Community Grants": manualLinear(
+    start,
+    start + periodToSeconds.year * 5,
+    172364192,
+  ),
   "Operations Grants": manualLinear(
     start,
     start + periodToSeconds.year * 5,
     qty * 0.114,
   ),
-  "New Issuance": newIssuance(),
+  "Staking Rewards": staking,
   meta: {
     notes: [
       `Community Sale tokens have a 12 to 24 month lockup. Here we have taken an average of 18 months lock up.`,
-      `87M of Early Ecosystem tokens (8.7% of total) have no set unlock schedule yet.`,
+      `Early Ecosystem (Unlocked) represents ~80M tokens provisioned for an unannounced distribution program. Early Ecosystem (Other) covers ~7M in miscellaneous grants with short lockups.`,
     ],
     sources: [`https://near.org/blog/near-token-supply-and-distribution`],
     token: `coingecko:near`,
@@ -80,8 +86,14 @@ const near: Protocol = {
     insiders: ["Core Team"],
     privateSale: ["Prior Backers 12m", "Prior Backers 24m", "Prior Backers 36m"],
     publicSale: ["Community Sale"],
-    staking: ["New Issuance"],
-    noncirculating: ["Foundation Endowment", "Operations Grants", "Early Ecosystem"],
+    staking: ["Staking Rewards"],
+    farming: [
+      "Community Grants",
+      "Early Ecosystem",
+      "Early Ecosystem (Unlocked)",
+      "Early Ecosystem (Other)",
+    ],
+    noncirculating: ["Foundation Endowment", "Operations Grants"],
   },
 };
 export default near;
